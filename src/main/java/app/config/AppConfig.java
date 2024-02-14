9b45e290febc2f351e580496ad5868487c84350b
Garfild1 <berngardtila@gmail.com>
2024-02-15 00:20:41 +0700

    first commit

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,18 @@
+package app.config;
+
+import app.model.Animal;
+import app.model.Dog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+
+
+
+}
---
+++ src/main/java/app/model/Animal.java
@@ -0,0 +1,8 @@
+package app.model;
+
+
+
+public abstract class Animal {
+
+
+}
